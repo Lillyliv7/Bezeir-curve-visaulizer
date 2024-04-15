b4a9e7972d68173840b8c6a05f9e2a43207f61e2
Lilly <60449924+Lillyliv7@users.noreply.github.com>
2024-04-14 20:47:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,17 +30,17 @@ while (run):
     # slope of point 2 and control point
     # slope2 = (point2[1] - controlPoint[1])/(point2[0] - controlPoint[0])
     B_past = point1Arr
-    for times in range(0, 100):
+    for times in range(0, 1000):
         # t = time/100
         # B(t) = (1-t)[(1-t)p0+tp1]+t[(1-t)p1+tp2]
-        t = times/100
+        t = times/1000
         B = (1-t)*((1-t)*point1+t*controlPoint)+t*((1-t)*controlPoint+t*point2)
         # window.set_at((round(B[0]), round(B[1])), 0xff0000)
         pygame.draw.line(window, 0xff0000, B, B_past)
         B_past = B
+    pygame.draw.line(window, 0xff0000, B_past, point2)
 
-
-        time.sleep(0.0001)
+        # time.sleep(0.0001)
 
     pygame.draw.circle(window, 0x00ff00, (point1[0], point1[1]), 5)
     pygame.draw.circle(window, 0x00ff00, (point2[0], point2[1]), 5)
